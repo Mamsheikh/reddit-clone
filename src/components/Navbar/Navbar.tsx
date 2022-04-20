2a712a84cf53548a187eaee41cabc83972bd6d05
Your Name <mamsheikh123@gmail.com>
2022-04-20 21:14:06 +0100

    💄 create community modal

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,14 +2,24 @@ import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/clientApp';
+import Directory from './Directory';
 import RightContent from './RightContent';
 import SearchInput from './SearchInput';
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg='white' height={'44px'} padding='6px 12px'>
-      <Flex align={'center'}>
+    <Flex
+      bg='white'
+      height={'44px'}
+      padding='6px 12px'
+      justify={{ md: 'space-between' }}
+    >
+      <Flex
+        align={'center'}
+        width={{ base: '40px', md: 'auto' }}
+        mr={{ base: 0, md: 2 }}
+      >
         <Image src='/images/redditFace.svg' alt='logo' height={'30px'} />
         <Image
           src='/images/redditText.svg'
@@ -18,9 +28,9 @@ const Navbar: React.FC = () => {
           display={{ base: 'none', md: 'unset' }}
         />
       </Flex>
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
-      {/* <Directory /> */}
     </Flex>
   );
 };
