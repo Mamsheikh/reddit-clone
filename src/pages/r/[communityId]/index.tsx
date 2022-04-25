3a4028d6c3d1582947a7e79d21a787f4a913d8fc
Your Name <mamsheikh123@gmail.com>
2022-04-25 14:29:28 +0100

    page content layout

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -6,6 +6,7 @@ import { firestore } from '../../../firebase/clientApp';
 import { Community } from '../../../atoms/communtiesAtom';
 import NotFound from '../../../components/Community/NotFound';
 import Header from '../../../components/Community/Header';
+import PageContent from '../../../components/Layout/PageContent';
 
 type CommunityPageProps = {
   communityData: Community;
@@ -18,6 +19,14 @@ const CommunitPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   return (
     <>
       <Header communityData={communityData} />
+      <PageContent>
+        <>
+          <div>LGS</div>
+        </>
+        <>
+          <div>RHS</div>
+        </>
+      </PageContent>
     </>
   );
 };
