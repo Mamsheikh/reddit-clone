487b9f84d64ac20121963ee0859a2eae07251c5b
Your Name <mamsheikh123@gmail.com>
2022-04-28 16:38:56 +0100

    show fetched posts in UI

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,14 +1,27 @@
+import { Stack } from '@chakra-ui/react';
 import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Community } from '../../atoms/communtiesAtom';
-import { firestore } from '../../firebase/clientApp';
+import { Post } from '../../atoms/postsAtom';
+import { auth, firestore } from '../../firebase/clientApp';
+import usePosts from '../../hooks/usePosts';
+import PostItem from './PostItem';
 
 type PostsProps = {
   communityData: Community;
 };
 
 const Posts: React.FC<PostsProps> = ({ communityData }) => {
+  const [user] = useAuthState(auth);
   const [loading, setLoading] = useState(false);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onVote,
+    onDeletePost,
+    onSelectPost,
+  } = usePosts();
   const getPosts = async () => {
     try {
       const postsQuery = query(
@@ -18,7 +31,11 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
       );
       const postsDoc = await getDocs(postsQuery);
       const posts = postsDoc.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      console.log('posts', posts);
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+      //   console.log('posts', posts);
     } catch (error: any) {
       console.log('getPosts error', error.message);
     }
@@ -27,6 +44,20 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
     getPosts();
   }, []);
 
-  return <div>Have a good coding</div>;
+  return (
+    <Stack>
+      {postStateValue.posts.map((post) => (
+        <PostItem
+          userIsCreator={user?.uid === post.creatorId}
+          userVoteValue={undefined}
+          post={post}
+          key={post.id}
+          onDeletePost={onDeletePost}
+          onVote={onVote}
+          onSelectPost={onSelectPost}
+        />
+      ))}
+    </Stack>
+  );
 };
 export default Posts;
