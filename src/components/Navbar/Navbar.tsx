b9436850bb8009c06bc966732be332332bf237b6
Your Name <mamsheikh123@gmail.com>
2022-05-07 22:06:02 +0100

    useDirectory hook and some touches

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,16 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { defaultMenuItem } from '../../atoms/directoryMenuAtom';
 import { auth } from '../../firebase/clientApp';
+import useDirectory from '../../hooks/useDirectory';
 import Directory from './Directory';
 import RightContent from './RightContent';
 import SearchInput from './SearchInput';
 
 const Navbar: React.FC = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const [user] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
   return (
     <Flex
       bg='white'
@@ -19,6 +22,8 @@ const Navbar: React.FC = () => {
         align={'center'}
         width={{ base: '40px', md: 'auto' }}
         mr={{ base: 0, md: 2 }}
+        cursor='pointer'
+        onClick={() => onSelectMenuItem(defaultMenuItem)}
       >
         <Image src='/images/redditFace.svg' alt='logo' height={'30px'} />
         <Image
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -24,7 +24,7 @@ const CommunitPage: React.FC<CommunityPageProps> = ({ communityData }) => {
       ...prev,
       currentCommunity: communityData,
     }));
-  }, []);
+  }, [communityData]);
 
   if (!communityData) {
     return <NotFound />;
